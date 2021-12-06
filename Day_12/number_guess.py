d563c5d75a206b648f48b0322d95ea38a9e3b836
Chris Birchard <zubazrichards@gmail.com>
2021-12-06 11:21:46 -0700

    changed some stuff

--- Day_12/number_guess.py
+++ Day_12/number_guess.py
@@ -1,5 +1,18 @@
 import random
 
+#Set a global variable so it's easy to change these values in the future.
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+#Instead of setting difficulty in play_game(), better to create function
+#The key here is to RETURN the number of guesses and then call it later
+def set_difficulty():
+    """returns the number of guesses based on difficulty chosen"""
+    level = input("Would you like to play easy or hard? ")
+    if level == "hard":
+        return HARD_LEVEL_TURNS
+    else:
+        return EASY_LEVEL_TURNS
 
 def play_again():
     """Checks to see if you would like to play again, if not thanks you for playing and ends game"""
@@ -11,21 +24,15 @@ def play_again():
         
 def play_game():
     game_over = False
-    num_guesses = 0
-    level = input("Would you like to play easy or hard? ")
-    if level == "hard":
-        num_guesses = 5
-    else:
-        num_guesses = 10
-
-    number = random.choice(range(1, 100))
+    num_guesses = set_difficulty() #This is critical learning, call the funtion to set the variable
+    number = random.randint(1, 100)
     guess = 0
 
     while game_over == False:
         if num_guesses <1:
-            print("You have no guesses left. ")
+            print("You have no guesses left. GAME OVER.")
             play_again()
-            game_over = True
+            game_over = True #I could use a return here and just jump out of the while loop
         else:
             print(f"You have {num_guesses} guesses left. ")
             guess = int(input("Please guess a number between 1 and 100. "))
@@ -39,11 +46,6 @@ def play_game():
                 print("Too low, please guess agian. ")
             num_guesses -= 1
 
+print("Welcome to the Number Guessing Game!")
 play_game()
-
-
-
-
-
-#TODO find ascii art
 
